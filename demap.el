a8cfa27876764799227e6f03aeb02b6d4784e3cc
sawyer gardner <sawyerjgardner@gmail.com>
2021-11-25 16:58:14 -0600

    basic setup

---
+++ demap.el
@@ -0,0 +1,27 @@
+;;; demap.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Sawyer Gardner
+;;
+;; Author: Sawyer Gardner <https://github.com/sawyer>
+;; Maintainer: Sawyer Gardner <sawyerjgardner@gmail.com>
+;; Created: November 25, 2021
+;; Modified: November 25, 2021
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/sawyer/demap
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+(defvar demap-buffers '())
+
+
+(provide 'demap)
+;;; demap.el ends here
