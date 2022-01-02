61a774a6b570e7d97b169f3257891792d21394d4
sawyer gardner <sawyerjgardner@gmail.com>
2022-01-01 22:29:49 -0600

    reformat track-w

--- demap.el
+++ demap.el
@@ -477,157 +477,182 @@ this is equivalent to (setf (`demap-minimap-showing' MINIMAP-OR-NAME) BUFFER-OR-
     minimap))
 
 
-;;;view struct-------
+;;;line-ov-------
 
-(cl-defstruct (demap-view
-               (:constructor demap--view-construct))
+(cl-defstruct (demap-line-ov
+               (:copier nil)
+               (:constructor nil)
+               (:constructor demap-line-ov-construct
+                (minimap
+                 &aux
+                 (overlay (make-overlay 0 0 (demap-minimap-buffer minimap))) )))
   (minimap nil :read-only t)
   (overlay nil :read-only t)
-  (line-ov nil :read-only t)
-  (window  nil)
-  (cleanup-func) )
+  (window  nil) )
 
 
-;;view line-ov
+;;line-ov update
 
-(defun demap-view-line-ov-update(view)
+(defun demap-line-ov-update(line-ov)
   ""
-  (if (eq (selected-window) (demap-view-window view))
-      (let ((ov      (demap-view-line-ov view))
-            (minimap (demap-view-minimap view)) )
-        (move-overlay ov (line-beginning-position) (+ (line-end-position) 1) (demap-minimap-buffer minimap)) )
-    (message "oh no")
-    (overlay-put (demap-view-line-ov view) 'face 'demap-current-line-inactive-face)
-    (remove-hook 'post-command-hook (apply-partially #'demap-view-line-ov-update view)) ))
+  (if (eq (selected-window) (demap-line-ov-window line-ov))
+      (move-overlay (demap-line-ov-overlay line-ov)
+                    (line-beginning-position)
+                    (+ (line-end-position) 1)
+                    (demap-minimap-buffer (demap-line-ov-minimap line-ov)) )
+    (demap-line-ov-unset line-ov) ))
 
-(defun demap-view-line-ov-update-reset(view)
+;;line-ov set
+
+(defun demap-line-ov-unset(line-ov)
   ""
-  (message "oh man")
-  (overlay-put (demap-view-line-ov view) 'face 'demap-current-line-face)
-  (add-hook 'post-command-hook (apply-partially #'demap-view-line-ov-update view))
-  (demap-view-line-ov-update view) )
+  (overlay-put (demap-line-ov-overlay line-ov) 'face 'demap-current-line-inactive-face)
+  (remove-hook 'post-command-hook (apply-partially #'demap-line-ov-update line-ov)) )
 
-
-;;view overlay
-
-(defun demap-view-overlay-update(view)
+(defun demap-line-ov-set(line-ov window)
   ""
-  (if (demap-view-window-active-p view)
-      (let ((ov      (demap-view-overlay view))
-            (window  (demap-view-window  view))
-            (minimap (demap-view-minimap view)) )
-        (move-overlay ov (window-start window) (window-end window t) (demap-minimap-buffer minimap)) )
-    (demap-view-cleanup-func-call view) ))
+  (when window
+    (overlay-put (demap-line-ov-overlay line-ov) 'face 'demap-current-line-face)
+    (setf (demap-line-ov-window line-ov) window)
+    (add-hook 'post-command-hook (apply-partially #'demap-line-ov-update line-ov))
+    (demap-line-ov-update line-ov) ))
 
-(defun demap-view-overlay-update-scroll(view window &optional start)
+
+;;;area-ov-------
+
+(cl-defstruct (demap-area-ov
+               (:copier nil)
+               (:constructor nil)
+               (:constructor demap-area-ov-construct
+                (minimap
+                 &aux
+                 (overlay (make-overlay 0 0 (demap-minimap-buffer minimap))) )))
+  (minimap nil :read-only t)
+  (overlay nil :read-only t)
+  (window  nil) )
+
+
+(defun demap-area-ov-active-p(area-ov)
   ""
-  (ignore start)
-  (when (eq window (demap-view-window view))
-    (demap-view-overlay-update view) ))
-
-(defun demap-view-overlay-update-resize(view &optional frame)
-  ""
-  (ignore frame)
-  (demap-view-overlay-update view) )
-
-
-;;view cleanup-func
-
-(defun demap-view-cleanup-func-call(view)
-  ""
-  (funcall (demap-view-cleanup-func view)) )
-
-(defun demap-view-cleanup-func-reset(view)
-  ""
-  (demap-view-cleanup-func-call view)
-  (let ((funca (apply-partially #'demap-view-overlay-update-scroll view))
-        (funcb (apply-partially #'demap-view-overlay-update-resize view))
-        (clean ) )
-    (demap--add-hook 'window-scroll-functions      funca)
-    (demap--add-hook 'window-size-change-functions funcb)
-    (setq clean (lambda()
-                  (demap--remove-hook 'window-scroll-functions      funca)
-                  (demap--remove-hook 'window-size-change-functions funcb) ))
-    (setf (demap-view-cleanup-func view) clean) ))
-
-
-;;view window
-
-(defun demap-view-window-active-p(view)
-  ""
-  (let ((window  (demap-view-window  view))
-        (minimap (demap-view-minimap view)) )
+  (let ((window  (demap-area-ov-window  area-ov))
+        (minimap (demap-area-ov-minimap area-ov)) )
     (and (window-live-p window)
          (eq (demap--real-buffer (window-buffer window)) (demap-minimap-showing minimap)) )))
 
-(defun demap-view-window-set(view window)
+;;area-ov update
+
+(defun demap-area-ov-update(area-ov &optional &rest r)
+  ""
+  (ignore r)
+  (let ((window (demap-area-ov-window area-ov)))
+    (if (demap-area-ov-active-p area-ov)
+        (move-overlay (demap-area-ov-overlay area-ov)
+                      (window-start window)
+                      (window-end window t)
+                      (demap-minimap-buffer (demap-area-ov-minimap area-ov)) )
+      (demap-area-ov-unset area-ov) )))
+
+(defun demap-area-ov-update-if-window(area-ov window &optional &rest r)
+  ""
+  (ignore r)
+  (when (eq (demap-area-ov-window area-ov) window)
+    (demap-area-ov-update area-ov) ))
+
+;;area-ov set
+
+(defun demap-area-ov-unset(area-ov)
+  ""
+  (let ((upd-if-finc (apply-partially #'demap-area-ov-update-if-window area-ov))
+        (update-func (apply-partially #'demap-area-ov-update area-ov)) )
+    (remove-hook 'window-scroll-functions      upd-if-finc)
+    (remove-hook 'window-size-change-functions update-func) ))
+
+(defun demap-area-ov-set(area-ov window)
   ""
   (if (not window)
-      (overlay-put (demap-view-overlay view) 'face 'demap-visible-region-inactive-face)
-    (overlay-put (demap-view-overlay view) 'face 'demap-visible-region-face)
-    (setf (demap-view-window view) window)
-    (setf (demap-minimap-showing (demap-view-minimap view)) (window-buffer window))
-    (demap-view-cleanup-func-reset view)
-    (demap-view-line-ov-update-reset view)
-    (demap-view-overlay-update view)
-    nil ))
+      (overlay-put (demap-area-ov-overlay area-ov) 'face 'demap-visible-region-inactive-face )
+    (overlay-put (demap-area-ov-overlay area-ov) 'face 'demap-visible-region-face)
+    (setf (demap-area-ov-window area-ov) window)
+    (let ((upd-if-finc (apply-partially #'demap-area-ov-update-if-window area-ov))
+          (update-func (apply-partially #'demap-area-ov-update area-ov)) )
+      (add-hook 'window-scroll-functions      upd-if-finc)
+      (add-hook 'window-size-change-functions update-func) )
+    (demap-area-ov-update area-ov) ))
 
 
-;;view update
+;;;track-w-------
 
-(defun demap-view-test-window-p(view);TODO: redo doc for all in view update
-  "Determin if demap-minimap VIEW can fallow the current window."
-  (ignore view)
+(cl-defstruct (demap-track-w
+               (:copier nil)
+               (:constructor nil)
+               (:constructor demap-track-w-construct-unhooked
+                (&optional
+                 (minimap (demap-minimap-construct))
+                 (line-ov (demap-line-ov-construct minimap))
+                 (area-ov (demap-area-ov-construct minimap)) )))
+  (minimap nil :read-only t)
+  (line-ov nil)
+  (area-ov nil) )
+
+
+(defun demap-track-w-test-window-p(track-w);TODO: redo doc for all in view update
+  "Determin if demap-minimap TRACK-W can fallow the current window."
+  (ignore track-w)
   (buffer-file-name (window-buffer)) )
 
-(defun demap-view-window-update(view)
-  "Update whet window demap-minimap VIEW is showing."
-  (demap-view-window-set view (when (demap-view-test-window-p view)
-                                (selected-window) )))
+;;track-w window
 
-(defun demap-view-smart-add-hook-window-update(view hook &optional depth local)
-  "Add VIEW's update function to HOOK and return cleanup function.
-adds a function that updates MINIMAP-OR-NAME to a
-normal hook HOOK and removes that function
-automatically when MINIMAP-OR-NAME is killed. this
-also returns a function that when called, removes
-the update function from HOOK now instead of later.
-DEPTH and LOCAL are passed to `add-hook'."
-  (let ((minimap     (demap-view-minimap view))
-        (update-func (apply-partially #'demap-view-window-update view))
-        (clean-func  )
-        (cclean-func ) )
-    (setq clean-func  (demap--smart-add-hook hook update-func depth local)
-          cclean-func (demap-smart-add-hook-minimap-local 'demap-minimap-kill-hook clean-func nil minimap) )
-    (lambda()
-      (funcall cclean-func)
-      (funcall clean-func) )))
-
-
-;;view construct
-
-(defun demap-view-construct(minimap)
+(defun demap-track-w-window-set(track-w window)
   ""
-  (let ((view)
-        (clean-call) )
-    (setq view (demap--view-construct :minimap      minimap
-                                      :overlay      (make-overlay 0 0 (demap-minimap-buffer minimap))
-                                      :line-ov      (make-overlay 0 0 (demap-minimap-buffer minimap))
-                                      :cleanup-func #'ignore ))
-    (setq clean-call (apply-partially #'demap-view-cleanup-func-call view) )
-    (demap-view-smart-add-hook-window-update view 'window-state-change-hook)
-    (demap--add-hook-local 'demap-minimap-kill-hook clean-call nil (demap-minimap-buffer minimap))
-    view ))
+  ;;TODO clean with when-let
+  (let ((minimap (demap-track-w-minimap track-w))
+        (line-ov (demap-track-w-line-ov track-w))
+        (area-ov (demap-track-w-area-ov track-w)) )
+    (when window
+      (setf (demap-minimap-showing minimap) (window-buffer window)) )
+    (when line-ov
+      (demap-line-ov-set line-ov window) )
+    (when area-ov
+      (demap-area-ov-set area-ov window) )))
 
+;;track-w update
 
+(defun demap-track-w-update(track-w)
+  "Update whet window demap-minimap TRACK-W is showing."
+  (demap-track-w-window-set track-w (when (demap-track-w-test-window-p track-w)
+                                      (selected-window) )))
 
-;;minimap make
+;;track-w hook
 
-(defun demap-view-make(&optional name)
-  "Construct a demap-minimap with name NAME."
-  (let ((overlay (demap-view-construct (demap-minimap-construct name))))
-    (demap-view-smart-add-hook-window-update overlay 'window-state-change-hook)
-    overlay ))
+(defun demap-track-w-unhook(track-w)
+  ""
+  (let ((update-func (apply-partially #'demap-track-w-update track-w))
+        (unhook-func (apply-partially #'demap-track-w-unhook track-w))
+        (buffer  (demap-minimap-buffer (demap-track-w-minimap track-w)))
+        (line-ov (demap-track-w-line-ov track-w))
+        (area-ov (demap-track-w-area-ov track-w)) )
+    (demap--remove-hook       'window-state-change-hook update-func)
+    (demap--remove-hook-local 'demap-minimap-kill-hook  unhook-func buffer)
+    (when line-ov
+      (demap-line-ov-unset line-ov) )
+    (when area-ov
+      (demap-area-ov-unset area-ov) ) ))
+
+(defun demap-track-w-hook(track-w)
+  ""
+  (let ((update-func (apply-partially #'demap-track-w-update track-w))
+        (unhook-func (apply-partially #'demap-track-w-unhook track-w))
+        (buffer      (demap-minimap-buffer (demap-track-w-minimap track-w))) )
+    (add-hook 'window-state-change-hook update-func)
+    (demap--add-hook-local 'demap-minimap-kill-hook unhook-func nil buffer) ))
+
+;;track-w construct
+
+(defun demap-track-w-construct()
+  ""
+  (let ((track-w (demap-track-w-construct-unhooked)))
+    (demap-track-w-hook track-w)
+    track-w))
 
 
 (provide 'demap)
