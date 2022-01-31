ffa5c2008e9c694a5b41942f02b0704cbd6c707e
sawyer gardner <sawyerjgardner@gmail.com>
2022-01-30 22:48:46 -0600

    added docs to demap.el

--- demap.el
+++ demap.el
@@ -28,7 +28,24 @@
 
 ;;;###autoload
 (defun demap-minimap-open(&optional minimap-or-name frame)
-  ""
+  "Open minimap in a side window.
+makes a minimap buffer and shows it. if
+MINIMAP-OR-NAME is non-nil or a minimap with the
+name in `demap-minimap-defalt-name' exists, show
+that minimap instead. if the minimap is already
+being shown, nuthing happens.
+
+FRAME specifies what frame to look for windows
+that already show the minimap. it should be a live
+frame or one of the fallowing:
+    nil      for the selected frame,
+    t        for any live frame,
+    'visible for any visible frame,
+    0        for any visible.
+
+the side and width of the new window can be set
+with `demap-set-open-options' or adding a display
+action to `display-buffer-alist' for minimaps."
   (interactive)
   (setq minimap-or-name (or minimap-or-name
                             (get-buffer demap-minimap-defalt-name)
@@ -43,7 +60,23 @@
 
 ;;;###autoload
 (defun demap-minimap-close(&optional minimap-or-name frame)
-  ""
+  "Close side window showing a minimap.
+close a side window showing MINIMAP-OR-NAME. has no
+effect on normal windows showing MINIMAP-OR-NAME.
+
+a side window is a window made by
+`display-buffer-in-side-window' (the defalt method
+used by `demap-minimap-open').
+
+FRAME specifies what frame to look for side windows
+showing a minimap. it should be a live frame or one
+of the fallowing:
+    nil      for the selected frame,
+    t        for any live frame,
+    'visible for any visible frame,
+    0        for any visible.
+
+if a window is removed returns t, otherwise nil."
   (interactive)
   (let* ((minimap (ignore-errors
                     (demap-normalize-minimap (or minimap-or-name
@@ -61,7 +94,20 @@
 
 ;;;###autoload
 (defun demap-minimap-toggle(&optional minimap-or-name frame)
-  ""
+  "Toggle side window showing a minimap.
+opens MINIMAP-OR-NAME in a side window. if its
+already showing, removes it instead.
+
+FRAME specifies what frame to look for side windows
+showing a minimap. it should be a live frame or one
+of the fallowing:
+    nil      for the selected frame,
+    t        for any live frame,
+    'visible for any visible frame,
+    0        for any visible.
+
+see `demap-minimap-open' and `demap-minimap-close'
+for more information."
   (interactive)
   (unless (demap-minimap-close minimap-or-name frame)
     (demap-minimap-open minimap-or-name frame)))
@@ -69,7 +115,16 @@
 
 
 (defun demap-set-open-options(&optional side width)
-  ""
+  "Set options for how to display a demap-minimap buffer.
+adds settings to `display-buffer-alist' for
+demap-minimap buffers, changing how
+`demap-minimap-open' opens the minimap.
+
+SIDE  is the side of the frame the window should be,
+        can be 'left or 'right (defalts to 'right).
+WIDTH is the width of the window, should be an
+        integer unit or a float percentage (defalts
+        to 20)."
   (setq side  (or side 'right)
         width (or width 20) )
   (let ((func (lambda(buffer act)
