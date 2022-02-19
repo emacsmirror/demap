08d0e7f7e7544f565c9b211c68c77a380c2b07e3
sawyer gardner <sawyerjgardner@gmail.com>
2022-02-18 20:29:39 -0600

    replaced demap-set-open-options

--- demap.el
+++ demap.el
@@ -45,6 +45,16 @@ buffer if it is not in any other window."
   :type  'boolean
   :group 'demap)
 
+(defcustom demap-minimap-window-side 'right
+  "The side of the frame `demap-minimap-open' opens a window on."
+  :type  '(radio (const right)
+                 (const left) )
+  :group 'demap)
+
+(defcustom demap-minimap-window-width 20
+  "The width of the window `demap-minimap-open' opens."
+  :type  'number
+  :group 'demap)
 
 ;;;###autoload
 (defun demap-minimap-open(&optional minimap-or-name frame)
@@ -61,22 +71,18 @@ frame or one of the fallowing:
     nil      for the selected frame,
     t        for any live frame,
     'visible for any visible frame,
-    0        for any visible.
-
-the side and width of the new window can be set
-with `demap-set-open-options' or adding a display
-action to `display-buffer-alist' for minimaps."
+    0        for any visible."
   (interactive)
-  (-> minimap-or-name
-      (or (get-buffer demap-minimap-defalt-name)
-          (demap-minimap-construct) )
-      (demap-normalize-minimap)
-      (demap-minimap-buffer)
-      (display-buffer '((display-buffer-in-side-window)
-                        (side          . right)
-                        (window-width  . 20)
-                        (preserve-size . (t . nil)) )
-                      frame )))
+  (let ((display-buffer-overriding-action '((display-buffer-in-side-window)
+                                            (side          . demap-minimap-window-side)
+                                            (window-width  . demap-minimap-window-width)
+                                            (preserve-size . (t . nil)) )))
+    (-> (or minimap-or-name
+            (get-buffer demap-minimap-defalt-name)
+            (demap-minimap-construct) )
+        (demap-normalize-minimap)
+        (demap-minimap-buffer)
+        (display-buffer nil frame) )))
 
 ;;;###autoload
 (defun demap-minimap-close(&optional minimap-or-name frame)
@@ -132,36 +138,8 @@ see `demap-minimap-open' and `demap-minimap-close'
 for more information."
   (interactive)
   (unless (demap-minimap-close minimap-or-name frame)
-    (demap-minimap-open minimap-or-name frame)))
+    (demap-minimap-open minimap-or-name frame) ))
 
-
-(defun demap-minimap-buffer-display-condition(buffer &optional act)
-  ""
-  (ignore act)
-  (demap-buffer-minimap buffer) )
-
-(defun demap-set-open-options(&optional side width)
-  "Set options for how to display a demap-minimap buffer.
-adds settings to `display-buffer-alist' for
-demap-minimap buffers, changing how
-`demap-minimap-open' opens the minimap.
-
-SIDE  is the side of the frame the window should be,
-        can be 'left or 'right (defalts to 'right).
-WIDTH is the width of the window, should be an
-        integer unit or a float percentage (defalts
-        to 20)."
-  (setq side  (or side 'right)
-        width (or width 20) )
-  (--> `,#'demap-minimap-buffer-display-condition
-       (assq-delete-all it display-buffer-alist)
-       (setq display-buffer-alist it))
-  (push `(,#'demap-minimap-buffer-display-condition
-          (display-buffer-in-side-window)
-          (side          . ,side)
-          (window-width  . ,width)
-          (preserve-size . (t . nil)) )
-        display-buffer-alist ))
 
 (provide 'demap)
 ;;; demap.el ends here
