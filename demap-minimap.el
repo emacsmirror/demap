5e1034abda4ffaf4bfe96bcc96144fa7396477eb
sawyer gardner <sawyerjgardner@gmail.com>
2022-03-14 19:10:38 -0500

    updated version number

--- demap-minimap.el
+++ demap-minimap.el
@@ -5,9 +5,10 @@
 ;; Author: Sawyer Gardner <https://gitlab.com/sawyerjgardner>
 ;; Created: January 03, 2022
 ;; Modified: March 9, 2022
-;; Version: 1.1.0
+;; Version: 1.3.0
 ;; Keywords: lisp convenience
 ;; Homepage: https://gitlab.com/sawyerjgardner/demap.el
+;; Package-Requires: ((emacs "24.4") (dash "2.18.0"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
